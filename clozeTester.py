e6cb663749eb5f0203d980bfe9019908b930ee8b
Tomas Ferrer <ferrer.t@outlook.com>
2017-11-02 14:57:19 -0400

    predicting

--- clozeTester.py
+++ clozeTester.py
@@ -3,56 +3,66 @@ from torch import cuda
 from torch.autograd import Variable
 from LSTMmodel import BiLSTMLM
 import dill
-
+import numpy as np
 import utils.tensor
 import utils.rand
 
-def get_lm_input(data):
-  input_data = []
-  for sent in data:
-    input_data.append(sent[:-1])
-  return input_data
+def embedding_to_word(embedding, vocab):
+    return ' '.join(vocab.itos[i] for i in embedding.data)
 
+_, _, test, vocab = torch.load(open("data/hw4_data.bin", 'rb'), pickle_module=dill)
 
-def get_lm_output(data):
-  output_data = []
-  for sent in data:
-    output_data.append(sent[1:])
-  return output_data
+batched_test, batched_test_mask = utils.tensor.advanced_batchize_no_sort(test, 1, vocab.stoi["<pad>"])
+#batched_test, batched_test_mask, _ = utils.tensor.advanced_batchize(test, 1, vocab.stoi["<pad>"])
 
-def main():
+vocab_size = len(vocab)
 
-  _, _, test, vocab = torch.load(open("data/hw4_data.bin", 'rb'), pickle_module=dill)
-  use_cuda = False
+bilstmlm = torch.load(open("LSTMmodel.py.nll_3.24.epoch_2", 'rb'), pickle_module=dill)
 
-  test_in = get_lm_input(test)
-  test_out = get_lm_output(test)
+bilstmlm.cpu()
 
-  batched_test_in, batched_test_in_mask, _ = utils.tensor.advanced_batchize(test_in, 1, vocab.stoi["<pad>"])
-  batched_test_out, batched_test_out_mask, _ = utils.tensor.advanced_batchize(test_out, 1, vocab.stoi["<pad>"])
-
-  vocab_size = len(vocab)
-  bilstmlm = torch.load(open("LSTMmodel.py.nll_3.24.epoch_2", 'rb'), pickle_module=dill)
-
-
-  for i, batch_i in enumerate(utils.rand.srange(len(batched_test_in))):
-      test_in_batch = Variable(batched_test_in[batch_i])  # of size (seq_len, batch_size)
-      test_out_batch = Variable(batched_test_out[batch_i])  # of size (seq_len, batch_size)
-      test_in_mask = Variable(batched_test_in_mask[batch_i])
-      test_out_mask = Variable(batched_test_out_mask[batch_i])
-
-      sys_out_batch = bilstmlm(test_in_batch)  # (seq_len, batch_size, vocab_size) # TODO: substitute this with your module
-      test_in_mask = test_in_mask.view(-1)
-      test_in_mask = test_in_mask.unsqueeze(1).expand(len(test_in_mask), vocab_size)
-      test_out_mask = test_out_mask.view(-1)
-      sys_out_batch = sys_out_batch.view(-1, vocab_size)
-      test_out_batch = test_out_batch.view(-1)
-      sys_out_batch = sys_out_batch.masked_select(test_in_mask).view(-1, vocab_size)
-      test_out_batch = test_out_batch.masked_select(test_out_mask)
-
-      print(test_out_batch)
-      
-
-
-if __name__ == "__main__":
-	main()
+bilstmlm.eval()
+m = 0
+for line in test:
+    #print(m, file=sys.stderr)
+    m += 1
+    blanks = []
+    for i in range(len(line)):
+        if vocab.itos[line[i]] == '<blank>':
+            blanks.append(i)
+    # print(blanks)
+    # print(line)
+    test_in = Variable(line).unsqueeze(1)
+    test_out = bilstmlm(test_in)
+    test_out = test_out.view(-1, vocab_size)
+    cur = []
+    newCur = []
+    for i in blanks:
+        # if vocab.itos[line[i]] == '<blank>':
+            # print(test_out[i][1:])
+        _, argmax = torch.max(test_out[i][1:], 0)
+        newCur.append(argmax.data[0] + 1)
+    count = 0
+    while newCur != cur and count < 20:
+        # print(7)
+        count += 1
+        # print(cur, newCur)
+        cur = newCur
+        newCur = []
+        # print(cur)
+        for j, i in enumerate(blanks):
+            # print(j, i, blanks)
+            line[i] = cur[j]
+        test_in = Variable(line).unsqueeze(1)
+        test_out = bilstmlm(test_in)
+        test_out = test_out.view(-1, vocab_size)
+        for i in blanks:
+            # if vocab.itos[line[i]] == '<blank>':
+                # print(test_out[i][1:])
+            _, argmax = torch.max(test_out[i][1:], 0)
+            newCur.append(argmax.data[0] + 1)
+        # print(line)
+    cur = []
+    for val in newCur:
+        cur.append(vocab.itos[val])
+    print(' '.join(cur).encode('utf-8').strip())
