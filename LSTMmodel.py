4396fab29fbd0b18c0e0f59af888656b3bbf6fa5
Tomas Ferrer <ferrer.t@outlook.com>
2017-11-02 09:36:11 -0400

    implement own LSTMCell

--- LSTMmodel.py
+++ LSTMmodel.py
@@ -3,6 +3,52 @@ import torch.nn as nn
 from torch.autograd import Variable
 import math
 
+class LSTM_Cell(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LSTM_Cell, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = 16
+        self.output_size = output_size
+
+        self.W_f = nn.Parameter(torch.Tensor(self.hidden_size, self.input_size + self.hidden_size))
+        self.b_f = nn.Parameter(torch.Tensor(self.hidden_size))
+
+        self.W_i = nn.Parameter(torch.Tensor(self.hidden_size, self.input_size + self.hidden_size))
+        self.b_i = nn.Parameter(torch.Tensor(self.hidden_size))
+
+        self.W_C = nn.Parameter(torch.Tensor(self.hidden_size, self.input_size + self.hidden_size))
+        self.b_C = nn.Parameter(torch.Tensor(self.hidden_size))
+
+        self.W_o = nn.Parameter(torch.Tensor(self.hidden_size, self.input_size + self.hidden_size))
+        self.b_o = nn.Parameter(torch.Tensor(self.hidden_size))
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.init_params()
+
+    def forward(self, input, ttuple):
+        hidden, C_previous_t = ttuple
+        combined = Variable(torch.cat((input.data, hidden.data), 1), requires_grad=True) # concatenate
+
+        f_t = self.sigmoid(combined.matmul(self.W_f.t()) + self.b_f)
+        i_t = self.sigmoid(combined.matmul(self.W_i.t()) + self.b_i)
+        C_tilde_t = torch.tanh(combined.matmul(self.W_C.t()) + self.b_C)
+        
+        C_t = f_t * C_previous_t + i_t * C_tilde_t
+
+        o_t = self.sigmoid(combined.matmul(self.W_o.t()) + self.b_o)
+
+        h_t = o_t * torch.tanh(C_t)
+
+        return h_t, C_t 
+
+    def init_params(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+
 
 class LSTMLM(nn.Module):
     def __init__(self, vocab_size):
@@ -12,7 +58,7 @@ class LSTMLM(nn.Module):
         self.hidden_size = 16
         self.vocab_size = vocab_size
         self.embedding = nn.Parameter(torch.randn(vocab_size, self.embedding_size))  # random word embeddin
-        self.lstm = nn.LSTMCell(self.embedding_size, self.hidden_size)
+        self.lstm = LSTM_Cell(self.embedding_size, self.hidden_size)
         self.h2o = nn.Linear(self.hidden_size, vocab_size)
         self.softmax = nn.LogSoftmax()
         self.init_params()
@@ -47,7 +93,7 @@ class BiLSTMLM(nn.Module):
         self.hidden_size = 16
         self.vocab_size = vocab_size
         self.embedding = nn.Parameter(torch.randn(vocab_size, self.embedding_size))  # random word embeddin
-        self.lstm = nn.LSTMCell(self.embedding_size, self.hidden_size)
+        self.lstm = LSTM_Cell(self.embedding_size, self.hidden_size)
         self.h2o = nn.Linear(self.hidden_size + self.hidden_size, vocab_size)
         self.softmax = nn.LogSoftmax()
         self.init_params()
