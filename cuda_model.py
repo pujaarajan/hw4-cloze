a375181cc01b80d75b58454755d050d18f6f4dd5
Tomas Ferrer <ferrer.t@outlook.com>
2017-11-02 21:57:28 -0400

    Add CUDA support

--- cuda_model.py
+++ cuda_model.py
@@ -15,30 +15,30 @@ class BiRNNLMwithDropout(nn.Module):
         # self.hidden_size = 30
         self.vocab_size = vocab_size
 
-        self.W_ih_lr = nn.Parameter(torch.Tensor(self.embedding_size + self.hidden_size, self.hidden_size)).cuda()
-        self.b_ih_lr = nn.Parameter(torch.Tensor(1, self.hidden_size)).cuda()
-        self.W_ih_rl = nn.Parameter(torch.Tensor(self.embedding_size + self.hidden_size, self.hidden_size)).cuda()
-        self.b_ih_rl = nn.Parameter(torch.Tensor(1, self.hidden_size)).cuda()
+        self.W_ih_lr = nn.Parameter(torch.Tensor(self.embedding_size + self.hidden_size, self.hidden_size).cuda())
+        self.b_ih_lr = nn.Parameter(torch.Tensor(1, self.hidden_size).cuda())
+        self.W_ih_rl = nn.Parameter(torch.Tensor(self.embedding_size + self.hidden_size, self.hidden_size).cuda())
+        self.b_ih_rl = nn.Parameter(torch.Tensor(1, self.hidden_size).cuda())
 
-        self.embedding = nn.Parameter(torch.randn(self.vocab_size, self.embedding_size)).cuda()  # random word embedding
-        self.W_ho = nn.Parameter(torch.Tensor(self.hidden_size * 2, self.vocab_size)).cuda()
-        self.b_ho = nn.Parameter(torch.Tensor(1, self.vocab_size)).cuda()
+        self.embedding = nn.Parameter(torch.randn(self.vocab_size, self.embedding_size).cuda())  # random word embedding
+        self.W_ho = nn.Parameter(torch.Tensor(self.hidden_size * 2, self.vocab_size).cuda())
+        self.b_ho = nn.Parameter(torch.Tensor(1, self.vocab_size).cuda())
 
         self.dropout_percent = 0.2
 
         self.softmax = nn.LogSoftmax()
 
-        self.initial_hidden = nn.Parameter(torch.Tensor(1, self.hidden_size)).cuda()
+        self.initial_hidden = nn.Parameter(torch.Tensor(1, self.hidden_size).cuda())
 
         self.init_params()
 
     def forward(self, input_batch, withDropout = True):
         seq_len, batch_size = input_batch.size()
-        predictions = Variable(torch.zeros(seq_len, batch_size, self.vocab_size), requires_grad=False).cuda()
-        hLR = Variable(torch.rand(seq_len + 1, batch_size, self.hidden_size), requires_grad=False).cuda()
-        hRL = Variable(torch.rand(seq_len + 1, batch_size, self.hidden_size), requires_grad=False).cuda()
+        predictions = Variable(torch.zeros(seq_len, batch_size, self.vocab_size).cuda(), requires_grad=False)
+        hLR = Variable(torch.rand(seq_len + 1, batch_size, self.hidden_size).cuda(), requires_grad=False)
+        hRL = Variable(torch.rand(seq_len + 1, batch_size, self.hidden_size).cuda(), requires_grad=False)
 
-        hidden = Variable(self.initial_hidden.data.expand(batch_size, self.hidden_size)).cuda()
+        hidden = Variable(self.initial_hidden.data.expand(batch_size, self.hidden_size).cuda())
         hLR[0,:,:] = hidden
 
         for t in xrange(seq_len):
@@ -49,11 +49,11 @@ class BiRNNLMwithDropout(nn.Module):
             hidden = hidden + self.b_ih_lr
             hidden = torch.tanh(hidden)
             if withDropout:
-                mask = Variable(torch.Tensor(np.random.binomial(np.ones(hidden.size(), dtype='int64'), 1-self.dropout_percent))).cuda()
+                mask = Variable(torch.Tensor(np.random.binomial(np.ones(hidden.size(), dtype='int64'), 1-self.dropout_percent)).cuda())
                 hidden = torch.mul(hidden, mask)
             hLR[t+1,:,:] = hidden
 
-        hidden = Variable(self.initial_hidden.data.expand(batch_size, self.hidden_size)).cuda()
+        hidden = Variable(self.initial_hidden.data.expand(batch_size, self.hidden_size).cuda())
         hRL[seq_len,:,:] = hidden
 
         for t in xrange(seq_len, 0, -1):
@@ -64,7 +64,7 @@ class BiRNNLMwithDropout(nn.Module):
             hidden = hidden + self.b_ih_rl
             hidden = torch.tanh(hidden)
             if withDropout:
-                mask = Variable(torch.Tensor(np.random.binomial(np.ones(hidden.size(), dtype='int64'), 1-self.dropout_percent))).cuda()
+                mask = Variable(torch.Tensor(np.random.binomial(np.ones(hidden.size(), dtype='int64'), 1-self.dropout_percent)).cuda())
                 hidden = torch.mul(hidden, mask)
             hRL[t-1,:,:] = hidden
 
