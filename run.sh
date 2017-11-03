f0853e6ee1fd3eeae3e4c6bff732f5134e57b602
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-11-02 20:31:17 -0400

    updated run script

--- run.sh
+++ run.sh
@@ -4,12 +4,15 @@
 # Example: ./run.sh 1
 #
 
-if [ $1 -eq 1 ] ; then
+if [ $1 == 1 ] ; then
     echo "running train.py"
     python train.py --data_file data/hw4_data.bin --optimizer Adam -lr 1e-2 --batch_size 48 --model_file model.py
-elif [ $1 -eq 2 ] ; then
+elif [ $1 == 2 ] ; then
     echo "running train_bi.py"
     python train_bi.py --data_file data/hw4_data.bin --optimizer Adam -lr 1e-2 --batch_size 48 --model_file model.py
+elif [ $1 == 3 ] ; then
+    echo "running train_dropout.py"
+    python train_dropout.py --data_file data/hw4_data.bin --optimizer Adam -lr 1e-2 --batch_size 48 --model_file model.py
 else
     echo "Unknown parameter"
 fi
