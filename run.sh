20492e07c616678101cfd2c3a2cb72ec1d4c6b5d
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-11-02 22:30:57 -0400

    added run script stuff for gpu

--- run.sh
+++ run.sh
@@ -13,6 +13,9 @@ elif [ $1 == 2 ] ; then
 elif [ $1 == 3 ] ; then
     echo "running train_dropout.py"
     python train_dropout.py --data_file data/hw4_data.bin --optimizer Adam -lr 1e-2 --batch_size 48 --model_file model.py
+elif [ $1 == 4 ] ; then
+    echo "running train_cuda_dropout.py"
+    python train_cuda_dropout.py --data_file data/hw4_data.bin --optimizer Adam -lr 1e-2 --batch_size 48 --model_file model.py --gpuid 1
 else
     echo "Unknown parameter"
 fi
